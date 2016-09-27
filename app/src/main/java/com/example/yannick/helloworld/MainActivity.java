1c10608716403f5a9e2f7ce483e968b6fc10dcfc
yannick <ymoreau42@gmail.com>
2016-09-27 16:57:49 +0200

    helloworld android

--- app/src/main/java/com/example/yannick/helloworld/MainActivity.java
+++ app/src/main/java/com/example/yannick/helloworld/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.yannick.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    Button bouton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=(TextView)findViewById(R.id.textView2);
+        bouton=(Button)findViewById(R.id.button);
+        bouton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Hello World");
+            }
+        });
     }
 }
